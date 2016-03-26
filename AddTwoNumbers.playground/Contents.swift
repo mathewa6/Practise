edc52df814ed3dfffe3a5f89fed822522d866c52
Adi <adimathew@ATCMac.local>
2016-03-26 17:19:43 -0400

    Added outline for LinkedList problem.

---
+++ AddTwoNumbers.playground/Contents.swift
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+ public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func addTwoNumbers(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        return ListNode(0)
+    }
+}
