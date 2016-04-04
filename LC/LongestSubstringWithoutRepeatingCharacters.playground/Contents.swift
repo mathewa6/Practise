05617c62ac3750ff4c6c35046771471dc427e447
Adi <mathewa6@users.noreply.github.com>
2016-04-03 22:04:08 -0400

    Moved leetCode problems to new LC folder. Started SubstringWithoutRepeating problem.

---
+++ LC/LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+//Insert each character into a set and then check for new characters existing in the set.
+class Solution {
+    func lengthOfLongestSubstring(s: String) -> Int {
+        
+    }
+}
