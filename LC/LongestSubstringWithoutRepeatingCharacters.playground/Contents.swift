783afa82d24c27ef0ec05ef5031f9c7ab36e75b9
Adi <mathewa6@users.noreply.github.com>
2016-04-04 21:48:20 -0400

    Incorrect double pass solution. Fails for sandwiched substrings.

--- LC/LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
+++ LC/LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
@@ -2,6 +2,36 @@
 //Insert each character into a set and then check for new characters existing in the set.
 class Solution {
     func lengthOfLongestSubstring(s: String) -> Int {
+        var bowl: Set<Character> = []
+        var count = s.isEmpty ? 0 : 1
         
+        var returnMax = count
+        
+        for (i, char) in s.characters.enumerate() {
+            if bowl.contains(char) {
+                count = bowl.count > count ? bowl.count : count
+                bowl.removeAll()
+            }
+            
+            bowl.insert(char)
+        }
+        returnMax =  max(count, bowl.count)
+        
+        bowl = []
+        count = s.isEmpty ? 0 : 1
+        for (i, char) in s.characters.enumerate().reverse() {
+            if bowl.contains(char) {
+                count = bowl.count > count ? bowl.count : count
+                bowl.removeAll()
+            }
+            
+            bowl.insert(char)
+        }
+        
+        return  max(count, returnMax)
     }
 }
+
+let sol = Solution()
+let input = "asjrgapa"
+sol.lengthOfLongestSubstring(input)
