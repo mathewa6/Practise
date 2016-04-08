83e3e5b7c3423fd018d408b56254f01e489d7558
Adi <mathewa6@users.noreply.github.com>
2016-04-07 23:29:31 -0400

    Added column sums to DP261.

--- r_DailyProgrammer/DailyProgrammer261.playground/Contents.swift
+++ r_DailyProgrammer/DailyProgrammer261.playground/Contents.swift
@@ -11,7 +11,9 @@ func isMagicSqaure(squareArray: Array<Int>, withSum sum: Int) -> Bool {
     let rowLength = Int(sqrt(Double(test.count)))
     var rowSum = sum
     var rowCount = rowLength
-
+    
+    var colSums = Array<Int>(count: rowLength, repeatedValue: sum)
+    
     for (i, element) in squareArray.enumerate() {
         rowSum -= element
         rowCount -= 1
@@ -23,8 +25,15 @@ func isMagicSqaure(squareArray: Array<Int>, withSum sum: Int) -> Bool {
                 rowCount = rowLength
             }
         }
+        
+        let currentColumn = i % rowLength
+        colSums[currentColumn] -= element
     }
     
+    if colSums.reduce(0, combine: +) != 0 {
+        return false
+    }
+
     return true
 }
 
