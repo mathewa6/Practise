8a952740e0ac68294866f57245f0370f7d15db1a
Adi <mathewa6@msu.edu>
2016-11-24 13:00:57 -0500

    Added gainlo.co playground with O(n) 2Sum implementation.

---
+++ Miscellany_blog_gainlo_co.playground/Contents.swift
@@ -0,0 +1,48 @@
+/// http://stackoverflow.com/questions/26395961/swift-array-element-address
+struct testStruc {
+    var _val = [Int](repeating: 0, count: 1000);
+    subscript(idx:Int) -> Int {
+        get {
+            print("get: [\(idx)] -> val(\(_val[idx]))")
+            return _val[idx]
+        }
+        set(val) {
+            print("set: [\(idx)] old(\(_val[idx])) -> new(\(val))")
+            _val[idx] = val
+        }
+    }
+}
+
+func testFunc( ptr:inout UnsafeMutablePointer<Int>, val:Int) {
+    print("mutating: ptr(\(ptr)) <- \(val)")
+    ptr.pointee = val
+}
+
+/// http://blog.gainlo.co/index.php/2016/07/19/3sum/
+func twoSum(forSum sum: Int, withArray array: [Int]) -> Bool {
+    
+    // Is array sorted? For now, assume it is
+    var a = 0
+    var b = array.count - 1
+    var s = array[a] + array[b]
+    var isTrue = false
+    
+    while a != b {
+        
+        if s == sum  {
+            isTrue = true
+            break
+        } else if s > sum {
+            b -= 1
+        } else {
+            a += 1
+        }
+        
+        s = array[a] + array[b]
+    }
+    
+    return isTrue
+}
+
+let testArray = [1, 5, 7]
+twoSum(forSum: 6, withArray: testArray)
