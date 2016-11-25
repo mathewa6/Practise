211130cb8a15cebc1d6c6e02d56df1f4e8f86d23
Adi <mathewa6@msu.edu>
2016-11-24 19:55:15 -0500

    Added some comments to clarify 2Sum.

--- Miscellany_blog_gainlo_co.playground/Contents.swift
+++ Miscellany_blog_gainlo_co.playground/Contents.swift
@@ -1,3 +1,7 @@
+
+/**
+ This section is irrelevant and was simply experimenting to see how raw pointers work in swift.
+ */
 /// http://stackoverflow.com/questions/26395961/swift-array-element-address
 struct testStruc {
     var _val = [Int](repeating: 0, count: 1000);
@@ -18,43 +22,52 @@ func testFunc( ptr:inout UnsafeMutablePointer<Int>, val:Int) {
     ptr.pointee = val
 }
 
+/**
+ twoSum(): Finds whether any 2 elements exactly add up to 'sum'.
+ threeSum(): Finds whether 3 elements add to 'sum' by calling twoSum().
+ */
 /// http://blog.gainlo.co/index.php/2016/07/19/3sum/
 func twoSum(forSum sum: Int, withArray array: [Int]) -> Bool {
     
     // Is array sorted? For now, assume it is
-    var a = 0
-    var b = array.count - 1
-    var s = array[a] + array[b]
-    var isTrue = false
     
+    var a = 0 // Leading index
+    var b = array.count - 1 // Trailing index
+    var s = array[a] + array[b] // Sum of elements at the leading and trailing indices
+    var exists = false
+    
+    // As long as the indices aren't meeting, iterate...
     while a != b {
         
+        // If the sum of elements at indices a & b equals sum, then the 2Sum exists.
         if s == sum  {
-            isTrue = true
+            exists = true
             break
         } else if s > sum {
             b -= 1
         } else {
             a += 1
         }
+        // Otherwise, pull the trailing back if a + b > sum
+        // Or push the leading forward if a + b < sum
         
         s = array[a] + array[b]
     }
     
-    return isTrue
+    return exists
 }
 
 func threeSum(forSum sum: Int, withArray array: [Int]) -> Bool {
-    var isTrue = false
+    var sumExists = false
     
     for x in array {
         let isTwoTrue = twoSum(forSum: sum-x, withArray: array)
         if isTwoTrue {
-            isTrue = true
+            sumExists = true
         }
     }
     
-    return isTrue
+    return sumExists
 }
 
 let testArray = [1, 5, 7]
