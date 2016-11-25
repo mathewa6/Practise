6323c0c0ce7e61ce9cf58088d2a0840560c8db3f
Adi <mathewa6@msu.edu>
2016-11-24 21:35:34 -0500

    Added explanations for returning the closest pair elements and removed print() from twoSum().

--- Miscellany_blog_gainlo_co.playground/Contents.swift
+++ Miscellany_blog_gainlo_co.playground/Contents.swift
@@ -33,12 +33,16 @@ func twoSum(forSum sum: Int, withArray array: [Int]) -> (Bool, (Int, Int)) {
     
     var a = 0 // Leading index
     var b = array.count - 1 // Trailing index
+    
+    // All variables below are not needed for loop iteration.
+    // They are used for the elements whose sum is closest to the target sum.
+    // x & y are the elements that are returned.
     var x = array[a]
     var y = array[b]
-    
     var s =  array[a] + array[b] // Sum of elements at the leading and trailing indices
-    var sdiff = abs(sum - s)
+    var sdiff = abs(sum - s) // The difference between target sum and s
     
+    // Final answer to whether the sum exists in the given array
     var exists = false
     
     // As long as the indices aren't meeting, iterate...
@@ -46,28 +50,45 @@ func twoSum(forSum sum: Int, withArray array: [Int]) -> (Bool, (Int, Int)) {
         
         // If the sum of elements at indices a & b equals sum, then the 2Sum exists.
         if s == sum  {
+            
             exists = true
             x = array[a]
             y = array[b]
+            
             break
+            
         } else if s > sum {
+            // Otherwise, pull the trailing back if a + b > sum
+
             b -= 1
+            
+            /**
+             If the diff between target sum and sum of the updated index' elements(LHS below) is <
+             the previous iteration's difference, ONLY then do we care to update x or y. We also
+             check for equals in the case of first iteration or genuine equality.
+            */
             if abs(sum - (array[a] + array[b])) <= abs(sdiff) {
+                
+                // If a == b, then retain old, since we do not want to return the same elements
                 y = a == b ? array[b+1] : array[b]
+                
                 s = array[a] + array[b]
                 sdiff = abs(sum - s)
+                
             }
         } else {
+            // Or push the leading forward if a + b < sum
+
             a += 1
-            print(abs(sum - (array[a] + array[b])), abs(sdiff))
+            
             if abs(sum - (array[a] + array[b])) <= abs(sdiff) {
+                
                 x = a == b ? array[a-1] : array[a]
+                
                 s = array[a] + array[b]
                 sdiff = abs(sum - s)
             }
         }
-        // Otherwise, pull the trailing back if a + b > sum
-        // Or push the leading forward if a + b < sum
         
 
     }
