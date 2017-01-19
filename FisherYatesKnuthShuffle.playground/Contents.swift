ea3cfe9e61306a1c0f93c91ccf5d31739ce53b8b
Adi <mathewa6@msu.edu>
2017-01-19 00:23:48 -0500

    Added random(range:) to FisherYates shuffle.

--- FisherYatesKnuthShuffle.playground/Contents.swift
+++ FisherYatesKnuthShuffle.playground/Contents.swift
@@ -1,11 +1,27 @@
 import Foundation
 
+// Borrowed from http://stackoverflow.com/questions/24132399/how-does-one-make-random-number-between-range-for-arc4random-uniform
+func random(range: Range<Int> ) -> Int {
+    var offset = 0
+    
+    if range.lowerBound < 0   // allow negative ranges
+    {
+        offset = abs(range.lowerBound)
+    }
+    
+    let lower = UInt32(range.lowerBound + offset)
+    let upper = UInt32(range.upperBound   + offset)
+    
+    return Int(lower + arc4random_uniform(upper - lower)) - offset
+}
+
 func shuffleFisherYates(array: [Int]) -> [Int] {
     
     var shuffledArray = array
     for idx in 0 ..< array.count {
-        let random = Int(arc4random_uniform(UInt32(array.count - idx))) + idx
-        (shuffledArray[idx], shuffledArray[random]) = (shuffledArray[random], shuffledArray[idx])
+        let randomIndex = random(range: idx ..< array.count)
+        // Int(arc4random_uniform(UInt32(array.count - idx))) + idx
+        (shuffledArray[idx], shuffledArray[randomIndex]) = (shuffledArray[randomIndex], shuffledArray[idx])
     }
     
     return shuffledArray
