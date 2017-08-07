732eaeb3c36c736ced2de26f072e0686a9b3985c
Adi <mathewa6@users.noreply.github.com>
2017-08-07 19:47:37 -0400

    Updated krazydadacolor.pgnd to add exporting to PNG of color strips and linear interpolation of color transitions.

--- krazydadacolor.playground/Contents.swift
+++ krazydadacolor.playground/Contents.swift
@@ -6,12 +6,12 @@ import PlaygroundSupport
 let frame: CGRect = CGRect(x: 0.0,
                            y: 0.0,
                            width: 750.0,
-                           height: 750.0)
+                           height: 37.5)
 let mainView: UIView = UIView(frame: frame)
 mainView.backgroundColor = .white
 
 
-var frequency: CGFloat = 0.1
+var frequency: CGFloat = 2 * .pi/40
 var amplitude: CGFloat = 128
 var center: CGFloat = 127
 
@@ -21,9 +21,9 @@ for i in 0..<count {
     let dim: CGFloat = frame.width/CGFloat(count)
     
     let subFrame: CGRect = CGRect(x: CGFloat(i) * dim,
-                                  y: CGFloat(i) * dim,
+                                  y: 0,
                                   width: dim,
-                                  height: frame.height/CGFloat(count))
+                                  height: 37.5)
     let tinyView: UIView = UIView(frame: subFrame)
     
     let phaseDefault: CGFloat = .pi/3
@@ -36,13 +36,61 @@ for i in 0..<count {
                                  green: vg,
                                  blue: vb,
                                  alpha: 1.0)
+    
     tinyView.backgroundColor = color
     mainView.addSubview(tinyView)
 }
 mainView
 PlaygroundPage.current.liveView = mainView
 
+let x = UIImage(view: mainView)
+
+let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+let filePath = "\(paths[0])/colorstair.png"
+
+print(filePath)
+
+let data: Data = UIImagePNGRepresentation(x)!
+do {
+    let url: URL = URL(fileURLWithPath: filePath)
+    try data.write(to: url)
+} catch let error as NSError {
+    print(error)
+}
+extension UIImage {
+    convenience init(view: UIView) {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, true, 0)
+        view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: (image?.cgImage)!)
+    }
+}
+
+public struct DXLColorComponents {
+    var red: CGFloat
+    var green: CGFloat
+    var blue: CGFloat
+    var alpha: CGFloat
+}
+
 extension UIColor {
+    
+    public var components: DXLColorComponents {
+        let componentArray: [CGFloat] = self.cgColor.components ?? [1, 1]
+        if componentArray.count == 2 {
+            return DXLColorComponents(red: componentArray[0],
+                                      green: componentArray[0],
+                                      blue: componentArray[0],
+                                      alpha: componentArray[1])
+        } else {
+            return DXLColorComponents(red: componentArray[0],
+                                      green: componentArray[1],
+                                      blue: componentArray[2],
+                                      alpha: componentArray[3])
+        }
+    }
+    
     static func generateColorSequence(length: Int) -> [UIColor] {
         var frequency: CGFloat = 0.1
         var amplitude: CGFloat = 128
@@ -64,7 +112,42 @@ extension UIColor {
         }
         return returnArray
     }
+    
+    static public func generateColorSequence(from firstColor: UIColor,
+                                             to secondColor: UIColor,
+                                             withLength length: Int) -> [UIColor] {
+        var returnArray: [UIColor] = []
+        for i in 0..<length {
+            
+            let color: UIColor = firstColor.lerp(to: secondColor,
+                                                 fraction: CGFloat(i+1)/CGFloat(length))
+            
+            returnArray.append(color)
+        }
+        return returnArray
+        
+    }
+    
+    public func lerp(to color: UIColor, fraction: CGFloat) -> UIColor {
+        fraction
+        var frac: CGFloat = max(0, fraction)
+        frac = min(1, fraction)
+        
+        let fromComponents: DXLColorComponents = self.components
+        let toComponents: DXLColorComponents = color.components
+        
+        let red: CGFloat = fromComponents.red + (toComponents.red - fromComponents.red) * frac
+        let green: CGFloat = fromComponents.green + (toComponents.green - fromComponents.green) * frac
+        let blue: CGFloat = fromComponents.blue + (toComponents.blue - fromComponents.blue) * frac
+
+        let alpha: CGFloat = fromComponents.alpha + (toComponents.alpha - fromComponents.alpha) * frac
+
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        
+    }
 }
 
 UIColor.generateColorSequence(length: 20)
+UIColor.generateColorSequence(from: .white, to: .black, withLength: 10)
 
